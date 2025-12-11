14dd984d087cf72c484c33542ceefcc46d62a19e
Marco Ricci <marco.ricci796@gmail.com>
2025-12-11 15:10:32 +0100

    add missing transactional annotation

--- src/main/java/com/relatech/warehouse_management_system/outbound/release/service/ReleaseService.java
+++ src/main/java/com/relatech/warehouse_management_system/outbound/release/service/ReleaseService.java
@@ -9,14 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-
-// filtri ordini
-// per data
-// per customer
-// per prodotto
-// ecc
 
 @Service
 @Slf4j
@@ -25,18 +20,22 @@ public class ReleaseService {
     private final CustomerService customerService;
     private final OrderService orderService;
 
+    @Transactional(readOnly = true)
     public Page<OrderDto> getOrdersByCustomer(String customerCode, Pageable pageable) {
         return orderService.filterByCustomer(customerCode, pageable);
     }
 
+    @Transactional(readOnly = true)
     public Page<OrderDto> getOrdersByDate(LocalDate start, LocalDate end, Pageable pageable) {
         return orderService.filterByDateRange(start, end, pageable);
     }
 
+    @Transactional(readOnly = true)
     public Page<OrderDto> getOrdersByProduct(Long productId, Pageable pageable) {
         return orderService.filterByProduct(productId, pageable);
     }
 
+    @Transactional(readOnly = true)
     public Page<OrderDto> getOrdersByParameters(String customerCode, Long productId, LocalDate start, LocalDate end, Pageable pageable) {
         return orderService.filterOrders(customerCode, productId, start, end, pageable);
     }
